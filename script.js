631d3842f5884c8504aa33fd0e972061df0f5071
Hamsa <hamsa.g@outlook.com>
2024-03-02 12:48:58 +0100

    Finish first Elzero challenge

---
+++ script.js
@@ -0,0 +1,9 @@
+let pageTitle = 'Elzero',
+  descriptionContent = 'Elzero Web School',
+  dateCurrent = '25/10';
+let diVar = `<div>
+    <h3>Hello ${pageTitle}</h3>
+    <p>${descriptionContent}</p>
+    <span>${dateCurrent}</span>
+  </div>`;
+document.write(diVar.repeat(4));
