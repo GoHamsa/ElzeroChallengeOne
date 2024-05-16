3076d2749192ed4c8dcdcfa5bd35c0962164b00e
Hamsa <hamsa.g@outlook.com>
2024-05-16 16:30:21 +0200

    Update Elzero

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-// 67 - Scope - Global And Local
+// 70 - Arrow Function Challenge
 /*
-  Scope
-  - Global And Local Scope
+  Function Arrow Challenges
 */
 
-var a = 1;
-let b = 2;
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
 
-function showText() {
-  var a = 10;
-  let b = 20;
-  console.log(`Function - From Local ${a}`);
-  console.log(`Function - From Local ${b}`);
-}
+let names = function () {
+  // Parameter ?
+  return '???';
+};
 
-console.log(`From Global ${a}`);
-console.log(`From Global ${b}`);
+console.log(names('Osama', 'Mohamed', 'Ali', 'Ibrahim'));
+// String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
 
-showText();
+/* ================================= */
+
+// [1] Replace ??? In Return Statement To Get The Output
+// [2] Create The Same Function With Regular Syntax
+// [3] Use Array Inside The Arguments To Get The Output
+
+let myNumbers = [20, 50, 10, 60];
+
+let calc = (one, two, ...nums) => '???';
+
+console.log(calc(10, '???', '???')); // 80
