bf26e6ff997b00e8b2f104f6480e14d9a79404fd
Hamsa <hamsa.g@outlook.com>
2024-05-16 10:33:01 +0200

    Update project

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-// 62 - Function Ultimate Practice
+// 63 - CHALLENGE Random Arguments Function
 /*
-  Function Advanced Practice
-  - Parameters
-  - Default
-  - Rest
-  - Loop
-  - Condition
+  Function - Random Argument Challenge
+  ====================================
+  Create Function showDetails
+  Function Accept 3 Parameters [a, b, c]
+  Data Types For Info Is
+  - String => Name
+  - Number => Age
+  - Boolean => Status
+  Argument Is Random
+  Data Is Not Sorted Output Depend On Data Types
+  - Use Ternary Conditional Operator
 */
 
-function showInfo(us = 'Un', ag = 'Un', rt = 0, show = 'Yes', ...sk) {
-  document.write(`<div>`);
-  document.write(`<h2>Welcome, ${us}</h2>`);
-  document.write(`<p>Age: ${ag}</p>`);
-  document.write(`<p>Hour Rate: $${rt}</p>`);
-  if (show === 'Yes') {
-    if (sk.length > 0) {
-      document.write(`<p>Skills: ${sk.join(' | ')}</p>`);
-    } else {
-      document.write(`<p>Skills: No Skills</p>`);
-    }
-  } else {
-    document.write(`<p>Skills Is Hidden</p>`);
-  }
-  document.write(`</div>`);
-}
-
-showInfo('Osama', 38, 20, 'No', 'Html', 'CSS');
+showDetails('Osama', 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(38, 'Osama', true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(true, 38, 'Osama'); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(false, 'Osama', 38); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
