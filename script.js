6a892ad33558d5110c1a892c4dfdeeced77c7853
Hamsa <hamsa.g@outlook.com>
2024-05-18 13:46:51 +0200

    Update 75

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-// 75 - Higher Order Functions - Reduce
+// 76 - Higher Order Functions - Reduce Practice
 
 /*
-  - Reduce
-  --- method executes a reducer function on each element of the array,
-  --- resulting in a single output value.
-
-  Syntax
-  reduce(callBackFunc(Accumulator, Current Val, Current Index, Source Array) { }, initialValue)
-  - Accumulator => the accumulated value previously returned in the last invocation
-  - Current Val => The current element being processed in the array
-  - Index => The index of the current element being processed in the array.
-  ---------- Starts from index 0 if an initialValue is provided.
-  ---------- Otherwise, it starts from index 1.
-  - Array => The Current Array
+  Reduce
+  - Longest Word
+  - Remove Characters + Use Reduce
 */
 
-let nums = [10, 20, 15, 30];
+let theBiggest = [
+  'Bla',
+  'Propaganda',
+  'Other',
+  'AAA',
+  'Battery',
+  'Test',
+  'Propaganda_Two',
+];
 
-let add = nums.reduce(function (acc, current, index, arr) {
+let check = theBiggest.reduce(function (acc, current) {
   console.log(`Acc => ${acc}`);
   console.log(`Current Element => ${current}`);
-  console.log(`Current Element Index => ${index}`);
-  console.log(`Array => ${arr}`);
-  console.log(acc + current);
+  console.log(acc.length > current.length ? acc : current);
   console.log(`#############`);
-  return acc + current;
-}, 5);
+  return acc.length > current.length ? acc : current;
+});
 
-console.log(add);
+console.log(check);
+
+let removeChars = ['E', '@', '@', 'L', 'Z', '@', '@', 'E', 'R', '@', 'O'];
+
+let finalString = removeChars
+  .filter(function (ele) {
+    return ele !== '@';
+  })
+  .reduce(function (acc, current) {
+    return `${acc}${current}`;
+  });
+
+console.log(finalString);
